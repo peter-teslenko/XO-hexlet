d702be4e207873c783aa8770626373eb9ea04deb
Peter Teslenko <smartchecker@gmail.com>
2015-11-26 16:26:01 +0300

    fix

--- tst/io/hexlet/xo/controllers/WinnerControllerTest.java
+++ tst/io/hexlet/xo/controllers/WinnerControllerTest.java
@@ -92,38 +92,30 @@ public class WinnerControllerTest {
     public void testGetWinnerWhenNoWinnerDiag1() throws Exception {
         final WinnerController winnerController = new WinnerController();
 
-        for (int i = 0; i < 3; i++) {
-            final Field field = new Field(3);
-            field.setFigure(new Point(0, 0), Figure.X);
-            field.setFigure(new Point(1, 1), Figure.X);
-            field.setFigure(new Point(2, 2), Figure.O);
-            assertNull(winnerController.getWinner(field));
-        }
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 2), Figure.O);
+        assertNull(winnerController.getWinner(field));
     }
 
     @Test
     public void testGetWinnerWhenWinnerDiag2() throws Exception {
         final WinnerController winnerController = new WinnerController();
-
-        for (int i = 0; i < 3; i++) {
-            final Field field = new Field(3);
-            field.setFigure(new Point(0, 2), Figure.X);
-            field.setFigure(new Point(1, 1), Figure.X);
-            field.setFigure(new Point(2, 0), Figure.X);
-            assertEquals(Figure.X, winnerController.getWinner(field));
-        }
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.X);
+        assertEquals(Figure.X, winnerController.getWinner(field));
     }
 
     @Test
     public void testGetWinnerWhenNoWinnerDiag2() throws Exception {
         final WinnerController winnerController = new WinnerController();
-
-        for (int i = 0; i < 3; i++) {
-            final Field field = new Field(3);
-            field.setFigure(new Point(0, 2), Figure.X);
-            field.setFigure(new Point(1, 1), Figure.X);
-            field.setFigure(new Point(2, 0), Figure.O);
-            assertNull(winnerController.getWinner(field));
-        }
+        final Field field = new Field(3);
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.O);
+        assertNull(winnerController.getWinner(field));
     }
 }
